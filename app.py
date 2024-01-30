88618bd570e54b5662f0152fa3b5c9040dee35b9
Umerfarooq02 <36485540+Umerfarooq02@users.noreply.github.com>
2024-01-29 22:30:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -26,13 +26,13 @@ def search_videos(query):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('/index.html')
 
 @app.route('/search', methods=['POST'])
 def search():
     query = request.form['query']
     videos = search_videos(query)
-    return render_template('index.html', videos=videos)
+    return render_template('/index.html', videos=videos)
 
 if __name__ == '__main__':
     app.run(debug=True)
